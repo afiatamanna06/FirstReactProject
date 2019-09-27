68f8f384fc9fa3415302a235937a847688c7c188
afiatamanna06 <afiatamanna06@gmail.com>
2019-09-27 20:05:47 +0600

    Event handler added

--- src/Components/counterComponent.jsx
+++ src/Components/counterComponent.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3']
     };
     styles = {
         fontSize: 10,
         fontWeight: "bold"
     };
+    handleIncrement() {
+        console.log("Increment clicked!");
+    }
+    /*renderTags() {
+        if(this.state.tags.length === 0) return <p>There are no tags!</p>;
+        return <ul>
+        { this.state.tags.map(tag => <li key = {tag}>{ tag }</li>)}
+     </ul>;
+    }*/
     render() { 
-        return <React.Fragment><h1>Hello World!</h1>
-        <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span><button className='btn btn-secondary btn-sm'>Increment</button></React.Fragment>;
+        return <React.Fragment>
+        <h1>Hello World!</h1>
+        <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span><button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
+        {/* this.state.tags.length === 0 && 'Please create a new tag!' */}
+        {/* this.renderTags() */}
+        </React.Fragment>;
     }
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
