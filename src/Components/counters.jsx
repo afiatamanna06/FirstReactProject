1cd146e8a39cde4dc4c4c56d5e39ea81d972d05c
afiatamanna06 <afiatamanna06@gmail.com>
2019-09-28 21:07:01 +0600

    render function modified in counters jsx

--- src/Components/counters.jsx
+++ src/Components/counters.jsx
@@ -15,7 +15,7 @@ class Counters extends Component {
     };
     render() { 
         return ( <div>
-            { this.state.counters.map(counter => <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} id={counter.id} />)}
+            { this.state.counters.map(counter => <Counter key={counter.id} onDelete={this.handleDelete} counter={counter} />)}
         </div> );
     }
 }
