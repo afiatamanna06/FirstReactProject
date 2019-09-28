c198fae0bdaccbca706d75e3af3ec2c5b62657d8
afiatamanna06 <afiatamanna06@gmail.com>
2019-09-28 20:43:31 +0600

    handleDelete function modified

--- src/Components/counters.jsx
+++ src/Components/counters.jsx
@@ -9,8 +9,9 @@ class Counters extends Component {
             { id: 4, value: 0 }
         ]
      };
-    handleDelete = () => {
-        console.log("Event handler called!")
+    handleDelete = counterId => {
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({ counters });
     };
     render() { 
         return ( <div>
