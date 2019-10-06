5e94a164bac6e535d9b46364c637c03193662538
afiatamanna06 <afiatamanna06@gmail.com>
2019-10-06 20:31:41 +0600

    Destructure of arguments added

--- src/Components/counters.jsx
+++ src/Components/counters.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import Counter from './counterComponent';
 class Counters extends Component {
     render() { 
+        const { onReset, counters, onIncrement, onDelete } = this.props;
         return ( <div>
-            <button onClick={this.props.onReset} className="btn btn-primary btn-sm m-2">Reset</button>
-            { this.props.counters.map(counter => <Counter key={counter.id} onIncrement={this.props.onIncrement} onDelete={this.props.onDelete} counter={counter} />)}
+            <button onClick={onReset} className="btn btn-primary btn-sm m-2">Reset</button>
+            { counters.map(counter => <Counter key={counter.id} onIncrement={onIncrement} onDelete={onDelete} counter={counter} />)}
         </div> );
     }
 }
--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-const NavBar = props => {
+const NavBar = ({ totalCounters }) => {
   return ( <nav className="navbar navbar-light bg-light">
-  <a className="navbar-brand" href="#">Navbar <span className="badge badge-pill badge-secondary">{ props.totalCounters }</span></a>
+  <a className="navbar-brand" href="#">Navbar <span className="badge badge-pill badge-secondary">{ totalCounters }</span></a>
   </nav> );
 };
 
