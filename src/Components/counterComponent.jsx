b693661735f8c825438e7d8ab2c7dedb82531e6d
afiatamanna06 <afiatamanna06@gmail.com>
2019-10-07 11:12:21 +0600

    Button style updated

--- src/Components/counterComponent.jsx
+++ src/Components/counterComponent.jsx
@@ -24,7 +24,7 @@ class Counter extends Component {
     render() { 
         return <div>
         <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={() => this.props.onIncrement(this.props.counter)} className='btn btn-secondary btn-sm'>Increment</button>
+        <button onClick={() => this.props.onIncrement(this.props.counter)} className='btn btn-secondary btn-sm'>+</button>
         <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
         {/* this.state.tags.length === 0 && 'Please create a new tag!' */}
         {/* this.renderTags() */}
@@ -38,7 +38,7 @@ class Counter extends Component {
 
     formatCount(){
         const { value: count } = this.props.counter;
-        return count === 0 ? "Zero" : count;
+        return count === 0 ? "0" : count;
     }
 }
  
