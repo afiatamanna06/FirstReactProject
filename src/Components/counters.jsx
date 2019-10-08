0ae69b3acc0d31b4d0869742456b61722e626a91
afiatamanna06 <afiatamanna06@gmail.com>
2019-10-08 19:57:33 +0600

    Decrement button added

--- src/Components/counters.jsx
+++ src/Components/counters.jsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import Counter from './counterComponent';
 class Counters extends Component {
     render() { 
-        const { onReset, counters, onIncrement, onDelete } = this.props;
+        const { onReset, counters, onIncrement,onDecrement, onDelete } = this.props;
         return ( <div>
             <button onClick={onReset} className="btn btn-primary btn-sm m-2">Reset</button>
-            { counters.map(counter => <Counter key={counter.id} onIncrement={onIncrement} onDelete={onDelete} counter={counter} />)}
+            { counters.map(counter => <Counter key={counter.id} onIncrement={onIncrement} onDecrement={onDecrement} onDelete={onDelete} counter={counter} />)}
         </div> );
     }
 }
