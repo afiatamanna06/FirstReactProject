350354da92cd14a3d8a70a0d5ace34321da21c76
afiatamanna06 <afiatamanna06@gmail.com>
2019-10-08 20:46:07 +0600

    style added at navbar

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 const NavBar = ({ totalCounters }) => {
-  return ( <nav className="navbar navbar-light bg-light">
-  <a className="navbar-brand" href="#">Navbar <span className="badge badge-pill badge-secondary">{ totalCounters }</span></a>
+  return ( <nav className="navbar navbar-light bg-info">
+  <a className="navbar-brand text-white" href="#">Navbar <span className="badge badge-pill badge-dark">{ totalCounters }</span></a>
   </nav> );
 };
 
